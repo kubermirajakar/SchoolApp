ae6e0ea6751d50291fd661880967b0a6161fd621
Kuber <kubermirajakar8@gmail.com>
2023-07-26 20:22:46 +0000

    removed colon

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -117,7 +117,6 @@ class _LoginScreenState extends State<LoginScreen> {
                           SizedBox(
                             height: kDefaultPadding,
                           ),
-                          s
                         ],
                       ),
                     )
